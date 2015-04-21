b352d08cc1031b3bd01a203b4218cc857341fa50
Marek Talarowski <marek.talarowski@gmail.com>
2015-04-21 09:46:58 +0200

    Opis Geterów i setterów

--- Główna/src/główna/Gra.java
+++ Główna/src/główna/Gra.java
@@ -84,67 +84,115 @@ public class Gra {
         this.opis = opis;
         this.wymagania = wymagania;
     }
-
+    /**
+     * pobierz tytuł
+     * @return wyświetl tytuł
+     */
     public String getTytuł() {
         return tytuł;
     }
-
+    /**
+     * ustaw tytuł
+     * @param tytuł przypisuje tytuł do tego parametru
+     */
     public void setTytuł(String tytuł) {
         this.tytuł = tytuł;
     }
-
+    /**
+     * pobierz rodzaj
+     * @return wyświetl rodzaj
+     */
     public rodzajGry[] getRodzaj() {
         return rodzaj;
     }
-
+    /**
+     * ustaw rodzaj 
+     * @param rodzaj przypisuje rodzaj do tego parametru
+     */
     public void setRodzaj(rodzajGry[] rodzaj) {
         this.rodzaj = rodzaj;
     }
-
+    /**
+     * pobierz Producenta 
+     * @return wyświetl producenta 
+     */
     public String getProducent() {
         return producent;
     }
-
+    /**
+     * ustaw producenta 
+     * @param producent przypisuje producenta do tego parametru
+     */
     public void setProducent(String producent) {
         this.producent = producent;
     }
-
+    /**
+     * pobierz wydawcę
+     * @return wyświetl wydawcę
+     */
     public String getWydawca() {
         return wydawca;
     }
-
+    /**
+     * ustaw wydawcę
+     * @param wydawca przypisuje wydawcę do tego parametru
+     */
     public void setWydawca(String wydawca) {
         this.wydawca = wydawca;
     }
-
+    /**
+     * pobierz date wydania
+     * @return wyświetl datę wydania
+     */
     public Data getDataWydania() {
         return dataWydania;
     }
-
+    /**
+     * ustaw datę wydania
+     * @param dataWydania przypisuje datę wydania do tego parametru
+     */
     public void setDataWydania(Data dataWydania) {
         this.dataWydania = dataWydania;
     }
-
+    /**
+     * pobierz wersję językowe
+     * @return wyświetl wersje językowe
+     */
     public String[] getWersjeJęzykowe() {
         return wersjeJęzykowe;
     }
-
+    /**
+     * ustaw wersje językowe
+     * @param wersjeJęzykowe przypisuje wersje językowe do tego parametru
+     */
     public void setWersjeJęzykowe(String[] wersjeJęzykowe) {
         this.wersjeJęzykowe = wersjeJęzykowe;
     }
-
+    /**
+     * pobierz opis
+     * @return wyświetl opis
+     */
     public String getOpis() {
         return opis;
     }
-
+    /**
+     * ustaw opis 
+     * @param opis przypisuje opis do tego parametru
+     */
     public void setOpis(String opis) {
         this.opis = opis;
     }
-
+    /**
+     * pobierz opis
+     * @return wyświetl opis
+     */
     public WymaganiaSystemowe getWymagania() {
         return wymagania;
     }
-
+    /**
+     * ustaw wymagania
+     * @param wymagania przypisuje wymagania do tego parametru
+     */
     public void setWymagania(WymaganiaSystemowe wymagania) {
         this.wymagania = wymagania;
     }
