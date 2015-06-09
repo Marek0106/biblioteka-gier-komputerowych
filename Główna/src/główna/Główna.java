7cfd3256f4b6171968b12a6543313ff58ee84bb5
Marek Talarowski <marek.talarowski@gmail.com>
2015-06-09 09:37:02 +0200

    Sterowanie

--- Główna/src/główna/Główna.java
+++ Główna/src/główna/Główna.java
@@ -9,6 +9,19 @@ public class Główna {
  */
     
     public static void main(String[] args) {
+        
+        /*
+        1.zapiszDoPliku
+        2.wczytajZPliku
+        3.dodaj
+        4.wyświetlListe
+        5.Usuń
+        6.WyświetlTytuł
+        7.WyświetlGryProducenta
+        8.WyświetlGryWydawcy
+        9.WyszukajGrę
+        if i=1 
+        */
 
     }
     
