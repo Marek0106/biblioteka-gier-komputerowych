c144864f5a5c0a8a3cd35fd4edff2416278a87dd
TalarowskiM <TalarowskiM@PWSZZ12.PWSZZ.pl>
2015-06-09 14:55:26 +0200

    dodany począteks sterowania

--- Główna/src/główna/Główna.java
+++ Główna/src/główna/Główna.java
@@ -1,28 +1,50 @@
 
 package główna;
 
+import javax.swing.JOptionPane;
+import sun.invoke.util.Wrapper;
+
 public class Główna {
 
-/**
- * 
- * @param args 
- */
-    
+    /**
+     *
+     * @param args
+     */
     public static void main(String[] args) {
-        
-        /*
-        1.zapiszDoPliku
-        2.wczytajZPliku
-        3.dodaj
-        4.wyświetlListe
-        5.Usuń
-        6.WyświetlTytuł
-        7.WyświetlGryProducenta
-        8.WyświetlGryWydawcy
-        9.WyszukajGrę
-        if i=1 
-        */
-
-    }
+        int numer;
     
+        
+        
+        String tmp1 = JOptionPane.showInputDialog(
+                "Wybierz numer z listy czynności którą chcesz wykonać\n"
+                + "        1.zapiszDoPliku\n"
+                + "        2.wczytajZPliku\n"
+                + "        3.dodaj\n"
+                + "        4.wyświetlListe\n"
+                + "        5.Usuń\n"
+                + "        6.WyświetlTytuł\n"
+                + "        7.WyświetlGryProducenta\n"
+                + "        8.WyświetlGryWydawcy\n"
+                + "        9.WyszukajGrę\n"
+                + "      ");
+           numer = Integer.parseInt(tmp1);
+
+
+
+    while (numer > 10 || numer < 0){
+
+        tmp1 = JOptionPane.showInputDialog("Ten numer nie mieści się w zakresie wyboru \n"
+                                                  + "wybierz inny numer.");
+
+        numer = Integer.parseInt(tmp1);
+
+      if(numer==1){
+          return
+           ;
+      }
+    }
+    }
 }
+          
+
+           
