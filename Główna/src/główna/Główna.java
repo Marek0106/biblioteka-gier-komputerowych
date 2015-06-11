c78baa0fe0172504b1cc78af2e526df64c906da0
Marek Talarowski <marek.talarowski@gmail.com>
2015-06-11 09:01:17 +0200

    Sterowanie

--- Główna/src/główna/Główna.java
+++ Główna/src/główna/Główna.java
@@ -1,8 +1,8 @@
-
 package główna;
 
+import java.io.IOException;
+
 import javax.swing.JOptionPane;
-import sun.invoke.util.Wrapper;
 
 public class Główna {
 
@@ -10,11 +10,11 @@ public class Główna {
      *
      * @param args
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         int numer;
-    
-        
-        
+
+        BibliotekaGier b = new BibliotekaGier(10);
+
         String tmp1 = JOptionPane.showInputDialog(
                 "Wybierz numer z listy czynności którą chcesz wykonać\n"
                 + "        1.zapiszDoPliku\n"
@@ -27,24 +27,46 @@ public class Główna {
                 + "        8.WyświetlGryWydawcy\n"
                 + "        9.WyszukajGrę\n"
                 + "      ");
-           numer = Integer.parseInt(tmp1);
-
-
-
-    while (numer > 10 || numer < 0){
-
-        tmp1 = JOptionPane.showInputDialog("Ten numer nie mieści się w zakresie wyboru \n"
-                                                  + "wybierz inny numer.");
-
         numer = Integer.parseInt(tmp1);
 
-      if(numer==1){
-          return
-           ;
-      }
-    }
+        while (numer > 9 || numer < 1) {
+
+            tmp1 = JOptionPane.showInputDialog("Ten numer nie mieści się w zakresie wyboru \n"
+                    + "wybierz inny numer.");
+
+            numer = Integer.parseInt(tmp1);
+
+            if (numer == 1) {
+                b.zapiszDoPliku("lista");
+            }
+
+            if (numer == 2) {
+                b.wczytajZPliku("lista");
+            }
+
+            if (numer == 3) {
+                b.dodaj(null);
+            }
+
+            if (numer == 4) {
+                b.wyświetlListe();
+            }
+
+     // if(numer==5){
+            //  b.Usuń();}
+            if (numer == 6) {
+                b.WyświetlTytuł();
+            }
+            if (numer == 7) {
+                b.WyświetlGryProducenta();
+            }
+            if (numer == 8) {
+                b.WyświetlGryWydawcy();
+            }
+            if (numer == 9) {
+                b.WyszukajGrę();
+            }
+
+        }
     }
 }
-          
-
-           
